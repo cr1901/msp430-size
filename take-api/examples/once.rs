904db5df2ed7457ce36bfe9032775fb3e7ed7e21
William D. Jones <thor0505@comcast.net>
2022-07-23 00:00:35 -0400

    Add example using OnceCell to detect size regressions.

---
+++ take-api/examples/once.rs
@@ -0,0 +1,35 @@
+#![no_main]
+#![no_std]
+#![feature(abi_msp430_interrupt)]
+#![deny(unsafe_code)]
+
+extern crate panic_msp430;
+
+use bare_metal::{CriticalSection, Mutex};
+use msp430_rt::entry;
+use msp430g2553::{interrupt, Peripherals};
+use once_cell::unsync::OnceCell;
+
+static PERIPHERALS: Mutex<OnceCell<Peripherals>> = Mutex::new(OnceCell::new());
+
+#[interrupt]
+fn TIMER0_A0(cs: CriticalSection) {
+    PERIPHERALS.borrow(cs).get().map(|p| &p.TIMER1_A3).unwrap();
+}
+
+#[interrupt]
+fn PORT1(cs: CriticalSection) {
+    PERIPHERALS.borrow(cs).get().map(|p| &p.PORT_1_2).unwrap();
+}
+
+fn init(cs: CriticalSection) {
+    let p = Peripherals::take().unwrap();
+    PERIPHERALS.borrow(cs).set(p).map_err(|_e| {}).unwrap();
+}
+
+#[entry(interrupt_enable(pre_interrupt = init))]
+fn main() -> ! {
+    loop {
+        
+    }
+}
