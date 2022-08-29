18c9691a3f0e1e2bee8d1b8838aa4b1b3df2e925
William D. Jones <thor0505@comcast.net>
2022-08-29 15:18:21 -0400

    Clean up critical crate a bit.

--- critical/src/lib.rs
+++ critical/src/lib.rs
@@ -3,59 +3,18 @@
 
 use cfg_if::cfg_if;
 
-cfg_if! {
-    if #[cfg(feature = "msp430")] {
-        use msp430::{register, interrupt};
-        use msp430::interrupt::CriticalSection;
-    } else {
-        use core::arch::asm;
-        use critical_section::CriticalSection;
-    }
+// critical crate provides the only provides acquire/release.
+#[no_mangle]
+pub unsafe fn acquire() -> u16 {
+    acquire_internal()
 }
 
-cfg_if! {
-    if #[cfg(feature = "msp430")] {
-        #[no_mangle]
-        pub unsafe fn acquire() -> u16 {
-            let sr = register::sr::read().bits();
-            interrupt::disable();
-            // Safety: Sr is repr(C), RawRestoreState is u16, and Sr contains
-            // only a single u16. This should be fine.
-            core::mem::transmute(sr)
-        }
-
-        #[no_mangle]
-        pub unsafe fn release(restore_state: u16) {
-            // Safety: Must be called w/ acquire, otherwise we could receive
-            // an invalid Sr (even though internally it's a u16, not all bits
-            // are actually used). It would be better to pass Sr as
-            // RawRestoreState, but since we can't, this will be acceptable,
-            // See acquire() for why this is safe.
-            let sr: register::sr::Sr = core::mem::transmute(restore_state);
-        
-            if sr.gie() {
-                interrupt::enable();
-            }
-        }
-    } else {
-        #[no_mangle]
-        pub unsafe fn acquire() -> u16 {
-            let fake_sr: u16 = 0;
-
-            let sr = core::ptr::read_volatile(&fake_sr as *const u16);
-            asm!("");
-            sr
-        }
-
-        #[no_mangle]
-        pub unsafe fn release(restore_state: u16) {
-            if restore_state != 0 {
-                asm!("");
-            }
-        }
-    }
+#[no_mangle]
+pub unsafe fn release(restore_state: u16) {
+    release_internal(restore_state)
 }
 
+// critical crate provides the free function as well as acquire/release.
 pub fn free<F, R>(f: F) -> R
 where
     F: FnOnce(CriticalSection) -> R,
@@ -70,20 +29,35 @@ where
 
 cfg_if! {
     if #[cfg(feature = "msp430")] {
+        use msp430::{register, interrupt};
+        use msp430::interrupt::CriticalSection;
+
         #[inline]
-        unsafe fn acquire_internal() -> register::sr::Sr {
+        unsafe fn acquire_internal() -> u16 {
             let status = register::sr::read();
             interrupt::disable();
-            status
+            // Safety: Sr is repr(C), RawRestoreState is u16, and Sr contains
+            // only a single u16. This should be fine.
+            core::mem::transmute(status)
         }
 
         #[inline]
-        unsafe fn release_internal(restore_state: register::sr::Sr) {
-            if restore_state.gie() {
+        unsafe fn release_internal(restore_state: u16) {
+            // Safety: Must be called w/ acquire, otherwise we could receive
+            // an invalid Sr (even though internally it's a u16, not all bits
+            // are actually used). It would be better to pass Sr as
+            // RawRestoreState, but since we can't, this will be acceptable,
+            // See acquire() for why this is safe.
+            let sr: register::sr::Sr = core::mem::transmute(restore_state);
+        
+            if sr.gie() {
                 interrupt::enable();
             }
         }
     } else {
+        use core::arch::asm;
+        use critical_section::CriticalSection;
+
         #[inline]
         unsafe fn acquire_internal() -> u16 {
             let fake_sr: u16 = 0;
@@ -93,7 +67,7 @@ cfg_if! {
             sr
         }
 
-        #[no_mangle]
+        #[inline]
         pub unsafe fn release_internal(restore_state: u16) {
             if restore_state != 0 {
                 asm!("");
